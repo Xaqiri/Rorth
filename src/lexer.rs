ff71c0fbe06002a293c6cc28cf11e434a18c860a
Xaqiri <xaqiri@outlook.com>
2024-02-22 18:41:01 -0600

    Wrote lexer for the calculator

---
+++ src/lexer.rs
@@ -0,0 +1,124 @@
+pub mod lexer {
+
+    #[derive(Debug, Clone, Copy)]
+    pub enum TokenType {
+        EOF,
+        INT(u32),
+        PLUS,
+        MINUS,
+        ASTERISK,
+        SLASH,
+        PERIOD,
+        COMMA,
+        EQUAL,
+    }
+
+    #[derive(Debug, Clone, Copy)]
+    pub struct Token {
+        col: usize,
+        row: usize,
+        pub tok_type: TokenType,
+    }
+
+    pub struct Lexer {
+        pos: usize,
+        col: usize,
+        row: usize,
+        peek: usize,
+        char: char,
+        pub tokens: Vec<Token>,
+        program: Vec<char>,
+    }
+
+    pub fn new(code: String) -> Lexer {
+        let mut l = Lexer {
+            pos: 0,
+            col: 0,
+            row: 0,
+            peek: 1,
+            program: code.chars().collect(),
+            char: ' ',
+            tokens: vec![],
+        };
+        l.char = l.program[l.pos];
+        l
+    }
+
+    impl Lexer {
+        pub fn print(self) {
+            for i in self.tokens.clone() {
+                println!("{:?}", i);
+            }
+        }
+        pub fn advance_token(&mut self) {
+            self.pos += 1;
+            self.col += 1;
+            self.peek += 1;
+            if self.pos >= self.program.len() {
+                self.char = '\0';
+            } else {
+                self.char = self.program[self.pos];
+            }
+        }
+
+        pub fn skip_space(&mut self) {
+            if self.program[self.pos] == '\n' {
+                self.row += 1;
+                self.advance_token();
+                self.col = 0;
+                self.skip_space();
+            }
+            if self.program[self.pos].is_whitespace() {
+                self.advance_token();
+                self.skip_space();
+            }
+        }
+
+        pub fn make_token(&self, token_type: TokenType) -> Token {
+            Token {
+                col: self.col,
+                row: self.row,
+                tok_type: token_type,
+            }
+        }
+
+        pub fn get_number(&mut self, i: char) {
+            let mut num = vec![];
+            num.push(i);
+            while self.program[self.peek].is_digit(10) {
+                num.push(self.program[self.peek]);
+                self.advance_token();
+            }
+            let s: String = num.iter().collect();
+            let n = s.parse::<u32>();
+            if let Ok(i) = n {
+                self.tokens.push(self.make_token(TokenType::INT(i)))
+            }
+        }
+
+        pub fn lex(&mut self) {
+            while self.char != '\0' {
+                self.skip_space();
+                match self.char {
+                    '+' => self.tokens.push(self.make_token(TokenType::PLUS)),
+                    '-' => self.tokens.push(self.make_token(TokenType::MINUS)),
+                    '*' => self.tokens.push(self.make_token(TokenType::ASTERISK)),
+                    '/' => self.tokens.push(self.make_token(TokenType::SLASH)),
+                    '.' => self.tokens.push(self.make_token(TokenType::PERIOD)),
+                    '=' => self.tokens.push(self.make_token(TokenType::EQUAL)),
+                    _ => {
+                        let n = self.char.to_digit(10);
+                        match n {
+                            Some(_) => self.get_number(self.char),
+                            None => println!(
+                                "main.rs: {}, {}: Error lexing character: {}",
+                                self.col, self.row, self.char
+                            ),
+                        }
+                    }
+                }
+                self.advance_token();
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,150 +1,138 @@
-use std::collections::HashSet;
+// use std::collections::HashSet;
 use std::fs::File;
 use std::io::{self, Write};
 use std::process::Command;
 
+use rorth::lexer::lexer::{new, TokenType};
+
 fn main() {
-    let program = "wow hi hello , , ,";
-    println!("main.rs: {}", program);
-    let mut num = "".to_string();
-    let mut str = "".to_string();
+    let program = "8 12 + 5 / 4 = .".to_string();
+    println!("main.rs: {:?}", program);
+
+    let mut l = new(program);
+    l.lex();
     let mut stack: Vec<f32> = vec![];
-    let mut string_stack: Vec<String> = vec![];
-    let mut string_heap = HashSet::new();
+    //     let mut string_stack: Vec<String> = vec![];
+    //     let mut string_heap = HashSet::new();
     let mut file = File::create("../out/rorth.ssa").unwrap();
     file.write(b"export function w $main() {\n@start\n")
         .unwrap();
-    for i in program.chars() {
-        if i.is_whitespace() {
-            if num == "" && str == "" {
-                continue;
-            } else if num != "" {
-                let x = num.parse::<f32>().unwrap();
-                stack.push(x);
-                let s = format!("\t%n{} =d add 0, d_{}\n", stack.len(), x);
+    for tok in l.tokens {
+        match tok.tok_type {
+            TokenType::PLUS => {
+                let a = stack.pop();
+                let b = stack.pop();
+                stack.push(b.unwrap_or(0.0) + a.unwrap_or(0.0));
+                let s = format!(
+                    "\t%n{} =d add %n{}, %n{}\n",
+                    stack.len(),
+                    stack.len(),
+                    stack.len() + 1
+                );
                 file.write(s.as_bytes()).unwrap();
-                num = "".to_string();
-                continue;
-            } else if str != "" {
-                string_stack.push(str.clone());
-                string_heap.insert(str);
-
-                str = "".to_string();
-                continue;
             }
-        }
-        if i.is_digit(10) {
-            num.push(i);
-        } else if i.is_alphabetic() {
-            str.push(i);
-        } else {
-            match i {
-                '+' => {
-                    let a = stack.pop();
-                    let b = stack.pop();
-                    stack.push(b.unwrap_or(0.0) + a.unwrap_or(0.0));
-                    let s = format!(
-                        "\t%n{} =d add %n{}, %n{}\n",
-                        stack.len(),
-                        stack.len(),
-                        stack.len() + 1
-                    );
-                    file.write(s.as_bytes()).unwrap();
-                }
-                '-' => {
-                    let a = stack.pop();
-                    let b = stack.pop();
-                    stack.push(b.unwrap_or(0.0) - a.unwrap_or(0.0));
-                    let s = format!(
-                        "\t%n{} =d sub %n{}, %n{}\n",
-                        stack.len(),
-                        stack.len(),
-                        stack.len() + 1
-                    );
-                    file.write(s.as_bytes()).unwrap();
-                }
-                '*' => {
-                    let a = stack.pop();
-                    let b = stack.pop();
-                    stack.push(b.unwrap_or(0.0) * a.unwrap_or(0.0));
-                    let s = format!(
-                        "\t%n{} =d mul %n{}, %n{}\n",
-                        stack.len(),
-                        stack.len(),
-                        stack.len() + 1
-                    );
-                    file.write(s.as_bytes()).unwrap();
-                }
-                '/' => {
-                    let a = stack.pop();
-                    let b = stack.pop();
-                    stack.push(b.unwrap_or(0.0) / a.unwrap_or(0.0));
-                    let s = format!(
-                        "\t%n{} =d div %n{}, %n{}\n",
-                        stack.len(),
-                        stack.len(),
-                        stack.len() + 1
-                    );
-                    file.write(s.as_bytes()).unwrap();
-                }
-                '=' => {
-                    let a = stack.pop().ok_or("No value on the stack");
-                    let b = stack.pop().ok_or("No value on the stack");
-                    match a == b {
-                        true => stack.push(1.0),
-                        false => stack.push(0.0),
-                    }
-                    let s = format!(
-                        "\t%b =w ceqd %n{}, %n{}\n\t%n{} =d swtof %b\n",
-                        stack.len(),
-                        stack.len() + 1,
-                        stack.len()
-                    );
-
-                    file.write(s.as_bytes()).unwrap();
-                }
-                '.' => {
-                    let a = stack.pop().ok_or(0);
-                    match a {
-                        Ok(n) => {
-                            let s: String;
-                            println!("main.rs: {}", n);
-                            if n == n.round() {
-                                s = format!(
-                                    "\tcall $printf(l $fmt_int, ..., d %n{})\n",
-                                    stack.len() + 1
-                                );
-                            } else {
-                                s = format!(
-                                    "\tcall $printf(l $fmt_dec, ..., d %n{})\n",
-                                    stack.len() + 1
-                                );
-                            }
-                            file.write(s.as_bytes()).unwrap();
-                        }
-                        Err(_) => {
-                            panic!("main.rs: Invalid '.': Nothing on the stack to print")
-                        }
-                    }
-                }
-                ',' => {
-                    let a = string_stack.pop().ok_or("");
-
-                    match a {
-                        Ok(n) => {
-                            let s: String;
-                            s = format!("\tcall $printf(l $fmt_str, ..., l $str_{})\n", n);
-                            file.write(s.as_bytes()).unwrap();
-                        }
-                        Err(_) => {
-                            panic!("main.rs: Invalid ',': Nothing on the stack to print");
-                        }
-                    }
-                }
-                _ => panic!("main.rs: Unknown value {}", i),
+            TokenType::MINUS => {
+                let a = stack.pop();
+                let b = stack.pop();
+                stack.push(b.unwrap_or(0.0) - a.unwrap_or(0.0));
+                let s = format!(
+                    "\t%n{} =d sub %n{}, %n{}\n",
+                    stack.len(),
+                    stack.len(),
+                    stack.len() + 1
+                );
+                file.write(s.as_bytes()).unwrap();
             }
+            TokenType::ASTERISK => {
+                let a = stack.pop();
+                let b = stack.pop();
+                stack.push(b.unwrap_or(0.0) * a.unwrap_or(0.0));
+                let s = format!(
+                    "\t%n{} =d mul %n{}, %n{}\n",
+                    stack.len(),
+                    stack.len(),
+                    stack.len() + 1
+                );
+                file.write(s.as_bytes()).unwrap();
+            }
+            TokenType::SLASH => {
+                let a = stack.pop();
+                let b = stack.pop();
+                stack.push(b.unwrap_or(0.0) / a.unwrap_or(0.0));
+                let s = format!(
+                    "\t%n{} =d div %n{}, %n{}\n",
+                    stack.len(),
+                    stack.len(),
+                    stack.len() + 1
+                );
+                file.write(s.as_bytes()).unwrap();
+            }
+            TokenType::PERIOD => {
+                let a = stack.pop().ok_or(0);
+                match a {
+                    Ok(n) => {
+                        let s: String;
+                        if n == n.round() {
+                            s = format!(
+                                "\tcall $printf(l $fmt_int, ..., d %n{})\n",
+                                stack.len() + 1
+                            );
+                        } else {
+                            s = format!(
+                                "\tcall $printf(l $fmt_dec, ..., d %n{})\n",
+                                stack.len() + 1
+                            );
+                        }
+                        file.write(s.as_bytes()).unwrap();
+                    }
+                    Err(_) => {
+                        panic!("main.rs: Invalid '.': Nothing on the stack to print")
+                    }
+                }
+            }
+            TokenType::COMMA => {
+                todo!()
+            }
+            TokenType::INT(i) => {
+                stack.push(i as f32);
+                let s = format!("\t%n{} =d add 0, d_{}\n", stack.len(), i);
+                file.write(s.as_bytes()).unwrap();
+            }
+            TokenType::EQUAL => {
+                let a = stack.pop().ok_or("No value on the stack");
+                let b = stack.pop().ok_or("No value on the stack");
+                match a == b {
+                    true => stack.push(1.0),
+                    false => stack.push(0.0),
+                }
+                let s = format!(
+                    "\t%b =w ceqd %n{}, %n{}\n\t%n{} =d swtof %b\n",
+                    stack.len(),
+                    stack.len() + 1,
+                    stack.len()
+                );
+
+                file.write(s.as_bytes()).unwrap();
+            }
+
+            TokenType::EOF => {}
         }
+        //                 ',' => {
+        //                     let a = string_stack.pop().ok_or("");
+
+        //                     match a {
+        //                         Ok(n) => {
+        //                             let s: String;
+        //                             s = format!("\tcall $printf(l $fmt_str, ..., l $str_{})\n", n);
+        //                             file.write(s.as_bytes()).unwrap();
+        //                         }
+        //                         Err(_) => {
+        //                             panic!("main.rs: Invalid ',': Nothing on the stack to print");
+        //                         }
+        //                     }
+        //                 }
     }
+
     file.write(b"\tret 0\n}\n").unwrap();
     file.write(b"data $fmt_int = { b \"%.f\\n\", b 0 }\n")
         .unwrap();
@@ -153,10 +141,10 @@ fn main() {
     file.write(b"data $fmt_str = { b \"%s\\n\", b 0 }\n")
         .unwrap();
 
-    for i in string_heap {
-        let s = format!("data $str_{} = {{ b \"{}\", b 0 }}\n", i, i);
-        file.write(s.as_bytes()).unwrap();
-    }
+    // for i in string_heap {
+    //     let s = format!("data $str_{} = {{ b \"{}\", b 0 }}\n", i, i);
+    //     file.write(s.as_bytes()).unwrap();
+    // }
 
     let cmd = Command::new("sh")
         .arg("-c")
